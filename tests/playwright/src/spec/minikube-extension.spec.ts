a995fbae99d6b1e1f6af2943127d8db137b065b1
Anton Misskii <amisskii@redhat.com>
2024-11-15 15:23:53 +0100

    fix: rebase/ fixes
    Signed-off-by: Anton Misskii <amisskii@redhat.com>

--- tests/playwright/src/spec/minikube-extension.spec.ts
+++ tests/playwright/src/spec/minikube-extension.spec.ts
@@ -1,8 +1,25 @@
+/**********************************************************************
+ * Copyright (C) 2023-2024 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
 import { 
     expect as playExpect, 
     ExtensionsPage,  
-    test, 
-    waitForPodmanMachineStartup, 
+    test,  
 } from '@podman-desktop/tests-playwright';
 
 const EXTENSION_IMAGE: string = 'ghcr.io/podman-desktop/podman-desktop-extension-minikube:nightly';
@@ -11,12 +28,11 @@ const EXTENSION_LABEL: string = 'podman-desktop.minikube';
 
 let extensionsPage: ExtensionsPage; 
 
-const skipExtensionInstallation = process.env.SKIP_EXTENSION_INSTALL ? process.env.SKIP_EXTENSION_INSTALL : false;
+const skipExtensionInstallation = process.env.SKIP_EXTENSION_INSTALL === 'true';
 
 test.beforeAll(async ({ runner, page, welcomePage }) => {
     runner.setVideoAndTraceName('minikube-extension-e2e');
     await welcomePage.handleWelcomePage(true);
-    await waitForPodmanMachineStartup(page);
     extensionsPage = new ExtensionsPage(page); 
 });
 
