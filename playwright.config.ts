eeeb21249fd96bbb7b62454ff7f83e27c123d223
Anton Misskii <124462506+amisskii@users.noreply.github.com>
2024-11-18 10:46:30 +0100

    test: Implement extension installation and basic extension handling test scenario (#209)
    * test: extension test scenario
    
    Signed-off-by: Anton Misskii <amisskii@redhat.com>
    
    * fix: fix
    
    Signed-off-by: Anton Misskii <amisskii@redhat.com>
    
    * fix: lint
    
    Signed-off-by: Anton Misskii <amisskii@redhat.com>
    
    * fix: rebase/ fixes
    
    Signed-off-by: Anton Misskii <amisskii@redhat.com>
    
    * Update minikube-extension.spec.ts
    
    Signed-off-by: Anton Misskii <124462506+amisskii@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Anton Misskii <amisskii@redhat.com>
    Signed-off-by: Anton Misskii <124462506+amisskii@users.noreply.github.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (C) 2023-2024 Red Hat, Inc.
+ * Copyright (C) 2024 Red Hat, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,24 +16,27 @@
  * SPDX-License-Identifier: Apache-2.0
  ***********************************************************************/
 
-import path from 'node:path';
+import { defineConfig, devices } from '@playwright/test';
 
-const excludeArray = [
-  'tests/playwright/**', '**/builtin/**',
-  '**/node_modules/**',
-  '**/dist/**',
-  '**/.{idea,git,cache,output,temp,cdix}/**',
-  '**/{.electron-builder,babel,changelog,docusaurus,jest,postcss,prettier,rollup,svelte,tailwind,vite,vitest*,webpack}.config.*',];
-  
-const config = {
-  test: {
-    exclude: excludeArray
-  },
-  resolve: {
-    alias: {
-      '@podman-desktop/api': path.resolve(__dirname, '__mocks__/@podman-desktop/api.js'),
+export default defineConfig({
+  testDir: 'tests/playwright/src/spec',
+  outputDir: 'tests/playwright/output/',
+  workers: 1,
+  timeout: 60000,
+
+  reporter: [
+    ['list'],
+    ['junit', { outputFile: 'tests/playwright/output/junit-results.xml' }],
+    ['json', { outputFile: 'tests/playwright/output/json-results.json' }],
+    ['html', { open: 'never', outputFolder: 'tests/playwright/output/html-results' }],
+  ],
+
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
     },
-  },
-};
-
-export default config;
+  ],
+});
---
+++ tests/playwright/src/spec/minikube-extension.spec.ts
@@ -0,0 +1,89 @@
+/**********************************************************************
+ * Copyright (C) 2024 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+import { 
+    expect as playExpect, 
+    ExtensionsPage,  
+    test,  
+} from '@podman-desktop/tests-playwright';
+
+const EXTENSION_IMAGE: string = 'ghcr.io/podman-desktop/podman-desktop-extension-minikube:nightly';
+const EXTENSION_NAME: string = 'minikube';
+const EXTENSION_LABEL: string = 'podman-desktop.minikube';
+
+let extensionsPage: ExtensionsPage; 
+
+const skipExtensionInstallation = process.env.SKIP_EXTENSION_INSTALL === 'true';
+
+test.beforeAll(async ({ runner, page, welcomePage }) => {
+    runner.setVideoAndTraceName('minikube-extension-e2e');
+    await welcomePage.handleWelcomePage(true);
+    extensionsPage = new ExtensionsPage(page); 
+});
+
+test.afterAll(async ({ runner }) => {
+    await runner.close();   
+});
+
+test.describe.serial('Podman Desktop Minikube Extension Tests', () => {
+
+    test('Install Minikube extension from OCI image', async ({ navigationBar }) => {
+        test.skip(!!skipExtensionInstallation, 'Skipping extension installation');
+        
+        await navigationBar.openExtensions();
+        await playExpect(extensionsPage.header).toBeVisible();
+        await playExpect.poll(async () => extensionsPage.extensionIsInstalled(EXTENSION_LABEL)).toBeFalsy();
+        await extensionsPage.openCatalogTab();
+        await extensionsPage.installExtensionFromOCIImage(EXTENSION_IMAGE);
+    });
+
+    test('Verify Minikube extension is installed and active', async ({ navigationBar }) => {
+        await navigationBar.openExtensions();
+        await playExpect(extensionsPage.header).toBeVisible();
+        await playExpect.poll(async () => extensionsPage.extensionIsInstalled(EXTENSION_LABEL)).toBeTruthy();
+        const minikubeExtension = await extensionsPage.getInstalledExtension(EXTENSION_NAME, EXTENSION_LABEL);
+        await playExpect(minikubeExtension.status).toHaveText('ACTIVE');
+    });
+
+    test('Ensure Minikube extension details page is correctly displayed', async () => {
+        const minikubeExtension = await extensionsPage.getInstalledExtension(EXTENSION_NAME, EXTENSION_LABEL);
+        const minikubeDetails = await minikubeExtension.openExtensionDetails('Minikube extension');
+        await playExpect(minikubeDetails.heading).toBeVisible();
+        await playExpect(minikubeDetails.status).toHaveText('ACTIVE');
+        await playExpect(minikubeDetails.tabContent).toBeVisible();
+    });
+
+    test('Ensure Minikube extension can be disabled and enabled', async ({ navigationBar }) => {
+        await navigationBar.openExtensions();
+        await playExpect(extensionsPage.header).toBeVisible();
+
+        const minikubeExtension = await extensionsPage.getInstalledExtension(EXTENSION_NAME, EXTENSION_LABEL);
+        await minikubeExtension.disableExtension();
+        await playExpect(minikubeExtension.enableButton).toBeEnabled();
+
+        await minikubeExtension.enableExtension();
+        await playExpect(minikubeExtension.disableButton).toBeEnabled();
+    });
+
+    test('Uninstall Minikube extension', async ({ navigationBar }) => {
+        await navigationBar.openExtensions();
+        await playExpect(extensionsPage.header).toBeVisible();
+        const minikubeExtension = await extensionsPage.getInstalledExtension(EXTENSION_NAME, EXTENSION_LABEL);
+        await minikubeExtension.removeExtension();
+    });
+});
